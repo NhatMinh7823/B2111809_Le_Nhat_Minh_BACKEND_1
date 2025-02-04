f51c2b8896ef3b08820dadcba0088e5223eb97fb
Le Nhat Minh <minhb2111809@student.ctu.edu.vn>
2025-02-04 19:33:28 +0700

    Dinh nghia cac Route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
+const contactsRouter = require("./app/routes/contact.route"); // Import the router
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/contacts", contactsRouter); // Register the contacts route
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
